f6d341e70bcfa0b5b376b758e66be2249ae07ca0
Nick Robinson <nr33@msstate.edu>
2014-11-30 15:12:39 -0600

    Checking in initial versions of tests

---
+++ src/burlap/testcode/BlockWorldTest.java
@@ -0,0 +1,325 @@
+package burlap.testcode;
+
+import java.util.*;
+
+import burlap.behavior.singleagent.EpisodeAnalysis;
+import burlap.behavior.singleagent.EpisodeSequenceVisualizer;
+import burlap.behavior.singleagent.Policy;
+import burlap.behavior.singleagent.auxiliary.StateReachability;
+import burlap.behavior.singleagent.learning.LearningAgent;
+import burlap.behavior.singleagent.learning.tdmethods.QLearning;
+import burlap.behavior.singleagent.planning.OOMDPPlanner;
+import burlap.behavior.singleagent.planning.QComputablePlanner;
+import burlap.behavior.singleagent.planning.StateConditionTest;
+import burlap.behavior.singleagent.planning.commonpolicies.GreedyQPolicy;
+import burlap.behavior.singleagent.planning.deterministic.DeterministicPlanner;
+import burlap.behavior.singleagent.planning.deterministic.SDPlannerPolicy;
+import burlap.behavior.singleagent.planning.deterministic.TFGoalCondition;
+import burlap.behavior.singleagent.planning.deterministic.uninformed.bfs.BFS;
+import burlap.behavior.singleagent.planning.stochastic.policyiteration.PolicyIteration;
+import burlap.behavior.singleagent.planning.stochastic.valueiteration.ValueIteration;
+import burlap.behavior.statehashing.NameDependentStateHashFactory;
+import burlap.behavior.statehashing.StateHashTuple;
+import burlap.debugtools.MyTimer;
+import burlap.domain.singleagent.blocksworld.BlocksWorld;
+import burlap.domain.singleagent.blocksworld.BlocksWorldVisualizer;
+import burlap.oomdp.auxiliary.StateParser;
+import burlap.oomdp.auxiliary.common.UniversalStateParser;
+import burlap.oomdp.core.Domain;
+import burlap.oomdp.core.GroundedProp;
+import burlap.oomdp.core.State;
+import burlap.oomdp.core.TerminalFunction;
+import burlap.oomdp.singleagent.RewardFunction;
+import burlap.oomdp.singleagent.SADomain;
+import burlap.oomdp.singleagent.common.UniformCostRF;
+import burlap.oomdp.visualizer.Visualizer;
+
+public class BlockWorldTest {
+
+    BlocksWorld							bwd;
+    Domain								domain;
+    StateParser 						sp;
+    RewardFunction 						rf;
+    TerminalFunction					tf;
+    StateConditionTest					goalCondition;
+    State 								initialState;
+    NameDependentStateHashFactory		hashingFactory;
+
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        BlockWorldTest rpt = new BlockWorldTest();
+        String outputPath = "output"; //directory to record results
+
+        //rpt.BFSExample(outputPath);
+        //rpt.ValueIterationExample(outputPath);
+        //rpt.PolicyIterationExample(outputPath);
+        rpt.QLearningExample(outputPath);
+
+        rpt.visualize(outputPath);
+
+
+        rpt.efficiencyTest();
+
+    }
+
+
+
+    public BlockWorldTest(){
+        bwd = new BlocksWorld();
+        domain = bwd.generateDomain();
+        sp = new UniversalStateParser(domain);
+        rf = new UniformCostRF();
+        tf = new StackTerminal();
+        goalCondition = new TFGoalCondition(tf);
+
+        initialState = BlocksWorld.getNewState(domain, 6);
+
+        hashingFactory = new NameDependentStateHashFactory();
+
+
+    }
+
+    public void visualize(String outputPath){
+        Visualizer v = BlocksWorldVisualizer.getVisualizer();
+        EpisodeSequenceVisualizer evis = new EpisodeSequenceVisualizer(v, domain, sp, outputPath);
+    }
+
+
+    public void BFSExample(String outputPath){
+
+        if(!outputPath.endsWith("/")){
+            outputPath = outputPath + "/";
+        }
+
+        //BFS ignores reward; it just searches for a goal condition satisfying state
+        DeterministicPlanner planner = new BFS(domain, goalCondition, hashingFactory);
+        planner.planFromState(initialState);
+
+        //capture the computed plan in a partial policy
+        Policy p = new SDPlannerPolicy(planner);
+
+        //record the plan results to a file
+        p.evaluateBehavior(initialState, rf, tf).writeToFile(outputPath + "planResult", sp);
+
+    }
+
+    public void ValueIterationExample(String outputPath){
+
+        if(!outputPath.endsWith("/")){
+            outputPath = outputPath + "/";
+        }
+
+
+        //Value iteration computing for discount=0.99 with stopping criteria either being a maximum change in value less then 0.001 or 100 passes over the state space (which ever comes first)
+        OOMDPPlanner planner = new ValueIteration(domain, rf, tf, 0.99, hashingFactory, 0.001, 100);
+        planner.planFromState(initialState);
+
+        //create a Q-greedy policy from the planner
+        Policy p = new GreedyQPolicy((QComputablePlanner)planner);
+
+        //record the plan results to a file
+        p.evaluateBehavior(initialState, rf, tf).writeToFile(outputPath + "planResult", sp);
+
+    }
+
+    public void PolicyIterationExample(String outputPath) {
+
+        if(!outputPath.endsWith("/")){
+            outputPath = outputPath + "/";
+        }
+
+        double start = System.nanoTime(), end = 0, evalTime = 0;
+        OOMDPPlanner planner = new PolicyIteration(domain, rf, tf, 0.5, hashingFactory, 0.001, 100, 1000);
+        planner.planFromState(initialState);
+        end = System.nanoTime();
+        evalTime = end - start;
+
+        evalTime /= Math.pow(10, 9);
+        System.out.println("Planner PI time: " + evalTime);
+        //create a Q-greedy policy from the planner
+        Policy p = new GreedyQPolicy((QComputablePlanner)planner);
+        start = System.nanoTime();
+        //record the plan results to a file
+        EpisodeAnalysis analysis = p.evaluateBehavior(initialState, rf, tf);
+        end = System.nanoTime();
+        evalTime = end - start;
+        evalTime /= Math.pow(10, 9);
+        LinkedList<State> uniqueStates = new LinkedList<State>();
+        System.out.println("Number of states to termination PI:" + analysis.stateSequence.size());
+        for(State s : analysis.stateSequence)
+        {
+            if(!uniqueStates.contains(s))
+                uniqueStates.add(s);
+        }
+        System.out.println("Unique states in policy PI: "+ uniqueStates.size());
+        System.out.println("PI time: " + evalTime);
+        System.out.println("Discount reward PI: " + analysis.getDiscountedReturn(0.99));
+        double totalReward = 0;
+
+        for( double r : analysis.rewardSequence)
+        {
+            totalReward += r;
+        }
+        System.out.println("Total reward PI: " + totalReward);
+        analysis.writeToFile(outputPath + "planResult", sp);
+        //this.visualizePolicy((QComputablePlanner) planner, p);
+    }
+
+    public void QLearningExample(String outputPath){
+
+        if(!outputPath.endsWith("/")){
+            outputPath = outputPath + "/";
+        }
+
+        //creating the learning algorithm object; discount= 0.99; initialQ=0.0; learning rate=0.9
+        LearningAgent agent = new QLearning(domain, rf, tf, 0.99, hashingFactory, 0., 0.9);
+
+        //run learning for 100 episodes
+        for(int i = 0; i < 200; i++){
+            EpisodeAnalysis ea = agent.runLearningEpisodeFrom(initialState); //run learning episode
+            ea.writeToFile(String.format("%se%03d", outputPath, i), sp); //record episode to a file
+            System.out.println(i + ": " + ea.numTimeSteps()); //print the performance of this episode
+        }
+
+    }
+
+
+    public class StackTerminal implements TerminalFunction{
+
+        List<GroundedProp> gps;
+
+        public StackTerminal(){
+            gps = new ArrayList<GroundedProp>();
+            gps.add(new GroundedProp(domain.getPropFunction(BlocksWorld.PFONBLOCK), new String[]{"block0", "block1"}));
+            gps.add(new GroundedProp(domain.getPropFunction(BlocksWorld.PFONBLOCK), new String[]{"block2", "block0"}));
+            gps.add(new GroundedProp(domain.getPropFunction(BlocksWorld.PFONBLOCK), new String[]{"block4", "block2"}));
+            gps.add(new GroundedProp(domain.getPropFunction(BlocksWorld.PFONBLOCK), new String[]{"block3", "block5"}));
+        }
+
+        @Override
+        public boolean isTerminal(State s) {
+            for(GroundedProp gp : gps){
+                if(!gp.isTrue(s)){
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+    }
+
+
+
+    public void efficiencyTest(){
+
+        Set <StateHashTuple> reachability;
+        MyTimer timer = new MyTimer();
+
+        System.out.println("Starting");
+
+        timer.start();
+        reachability = StateReachability.getReachableHashedStates(initialState, (SADomain)domain, hashingFactory);
+        timer.stop();
+
+        System.out.println("Reachability Time: " +  timer.getTime() + "; n states: " + reachability.size());
+
+        Set <StateHashTuple> copiedSet = new HashSet<StateHashTuple>(reachability.size());
+        timer.start();
+        for(StateHashTuple sh : reachability){
+            StateHashTuple nsh = this.hashingFactory.hashState(sh.s.copy());
+            copiedSet.add(nsh);
+        }
+        timer.stop();
+        System.out.println("Set Copy Time: " + timer.getTime() + "; n states: " + copiedSet.size());
+
+        List<State> flatList = new ArrayList<State>();
+        timer.start();
+        for(StateHashTuple sh : reachability){
+            State ns = sh.s.copy();
+            flatList.add(ns);
+        }
+        timer.stop();
+        System.out.println("Flat List Time: " + timer.getTime() + "; n states: " + flatList.size());
+
+        timer.start();
+        for(StateHashTuple sh : reachability){
+            StateHashTuple nsh = this.hashingFactory.hashState(sh.s.copy());
+            nsh.hashCode(); //forces hashCode value compute
+        }
+        timer.stop();
+        System.out.println("Hash Time: " + timer.getTime() + "; n states: " + copiedSet.size());
+
+        Set <Integer> hashCodes = new HashSet<Integer>();
+        timer.start();
+        for(StateHashTuple sh : reachability){
+            StateHashTuple nsh = this.hashingFactory.hashState(sh.s.copy());
+            int hc = nsh.hashCode(); //forces hashCode value compute
+            hashCodes.add(hc);
+        }
+        timer.stop();
+        System.out.println("Hash Code Index Time: " + timer.getTime() + "; n hash codes: " + hashCodes.size());
+
+		/*for(int hc : hashCodes){
+			System.out.println(hc);
+		}*/
+
+
+    }
+
+
+    public void efficiencyTest2(){
+
+        Set <StateHashTuple> reachability;
+        MyTimer timer = new MyTimer();
+
+        System.out.println("Starting");
+
+        timer.start();
+        reachability = StateReachability.getReachableHashedStates(initialState, (SADomain)domain, hashingFactory);
+        timer.stop();
+
+        System.out.println("Time: " +  timer.getTime() + "; n states: " + reachability.size());
+
+
+    }
+
+
+    public void efficiencyTest1(){
+
+        int n = 24306;
+        MyTimer timer = new MyTimer();
+
+
+        List<StateHashTuple> shList = new ArrayList<StateHashTuple>();
+        timer.start();
+        for(int i = 0; i < n; i++){
+            State s = initialState.copy();
+            StateHashTuple sh = this.hashingFactory.hashState(s);
+            sh.hashCode();
+            shList.add(sh);
+        }
+
+        timer.stop();
+        System.out.println("List time: " + timer.getTime());
+        System.out.println(shList.size());
+
+        Set<StateHashTuple> shSet = new HashSet<StateHashTuple>();
+        timer.start();
+        for(int i = 0; i < n; i++){
+            State s = initialState.copy();
+            StateHashTuple sh = this.hashingFactory.hashState(s);
+            shSet.add(sh);
+        }
+
+        timer.stop();
+        System.out.println("Set time: " + timer.getTime());
+        System.out.println(shSet.size());
+
+    }
+
+
+}
---
+++ src/burlap/testcode/Maze.java
@@ -0,0 +1,307 @@
+package burlap.testcode;
+
+import java.awt.Color;
+import java.util.LinkedList;
+import java.util.List;
+
+import burlap.behavior.singleagent.EpisodeAnalysis;
+import burlap.behavior.singleagent.Policy;
+import burlap.behavior.singleagent.auxiliary.StateReachability;
+import burlap.behavior.singleagent.auxiliary.performance.LearningAlgorithmExperimenter;
+import burlap.behavior.singleagent.auxiliary.performance.PerformanceMetric;
+import burlap.behavior.singleagent.auxiliary.performance.TrialMode;
+import burlap.behavior.singleagent.auxiliary.valuefunctionvis.ValueFunctionVisualizerGUI;
+import burlap.behavior.singleagent.auxiliary.valuefunctionvis.common.ArrowActionGlyph;
+import burlap.behavior.singleagent.auxiliary.valuefunctionvis.common.LandmarkColorBlendInterpolation;
+import burlap.behavior.singleagent.auxiliary.valuefunctionvis.common.PolicyGlyphPainter2D;
+import burlap.behavior.singleagent.auxiliary.valuefunctionvis.common.PolicyGlyphPainter2D.PolicyGlyphRenderStyle;
+import burlap.behavior.singleagent.auxiliary.valuefunctionvis.common.StateValuePainter2D;
+import burlap.behavior.singleagent.learning.LearningAgent;
+import burlap.behavior.singleagent.learning.LearningAgentFactory;
+import burlap.behavior.singleagent.learning.tdmethods.QLearning;
+import burlap.behavior.singleagent.planning.OOMDPPlanner;
+import burlap.behavior.singleagent.planning.QComputablePlanner;
+import burlap.behavior.singleagent.planning.StateConditionTest;
+import burlap.behavior.singleagent.planning.commonpolicies.GreedyQPolicy;
+import burlap.behavior.singleagent.planning.deterministic.TFGoalCondition;
+import burlap.behavior.singleagent.planning.stochastic.policyiteration.PolicyIteration;
+import burlap.behavior.singleagent.planning.stochastic.valueiteration.ValueIteration;
+import burlap.behavior.statehashing.DiscreteStateHashFactory;
+import burlap.domain.singleagent.gridworld.GridWorldDomain;
+import burlap.domain.singleagent.gridworld.GridWorldStateParser;
+import burlap.oomdp.auxiliary.StateGenerator;
+import burlap.oomdp.auxiliary.StateParser;
+import burlap.oomdp.auxiliary.common.ConstantStateGenerator;
+import burlap.oomdp.core.Domain;
+import burlap.oomdp.core.State;
+import burlap.oomdp.core.TerminalFunction;
+import burlap.oomdp.singleagent.RewardFunction;
+import burlap.oomdp.singleagent.SADomain;
+import burlap.oomdp.singleagent.common.SinglePFTF;
+import burlap.oomdp.singleagent.common.UniformCostRF;
+
+public class Maze {
+    private static final boolean POLICY_VIS_ON = true;
+    // The probability that our agent moves in the correct direction
+    private static final double probSucceed = 0.8;
+
+    GridWorldDomain				gwdg;
+    Domain						domain;
+    StateParser					sp;
+    RewardFunction				rf;
+    TerminalFunction			tf;
+    StateConditionTest			goalCondition;
+    State						initialState;
+    DiscreteStateHashFactory	hashingFactory;
+
+    int myMap[][] = {
+            {1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,0,1,0,1,0,1,0,0,0,0,0,1},
+            {1,0,1,0,0,0,1,0,1,1,1,0,1},
+            {1,0,0,0,1,1,1,0,0,0,0,0,1},
+            {1,0,1,0,0,0,0,0,1,1,1,0,1},
+            {1,0,1,0,1,1,1,0,1,0,0,0,1},
+            {1,0,1,0,1,0,0,0,1,1,1,0,1},
+            {1,0,1,0,1,1,1,0,1,0,1,0,1},
+            {1,0,0,0,0,0,0,0,0,0,1,0,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1}
+    };
+
+    public Maze(){
+        // Setup our Grid World Domain
+        gwdg = new GridWorldDomain(13, 13);
+        gwdg.setProbSucceedTransitionDynamics(probSucceed);
+        gwdg.setMap(myMap);
+        domain = gwdg.generateDomain();
+
+        //Setup initial state of our agent and reward location
+        initialState = GridWorldDomain.getOneAgentOneLocationState(domain);
+        GridWorldDomain.setAgent(initialState, 1, 1);
+        GridWorldDomain.setLocation(initialState, 0, 1, 8);
+
+        sp = new GridWorldStateParser(domain);
+        rf = new UniformCostRF();
+        tf = new SinglePFTF(domain.getPropFunction(GridWorldDomain.PFATLOCATION));
+        goalCondition = new TFGoalCondition(tf);
+        hashingFactory = new DiscreteStateHashFactory();
+        hashingFactory.setAttributesForClass(GridWorldDomain.CLASSAGENT,
+                domain.getObjectClass(GridWorldDomain.CLASSAGENT).attributeList);
+//        VisualActionObserver observer = new VisualActionObserver(domain,
+//        		GridWorldVisualizer.getVisualizer(gwdg.getMap()));
+//        ((SADomain)this.domain).setActionObserverForAllAction(observer);
+//        observer.initGUI();
+    }
+
+    public static void main(String[] args) {
+
+        Maze testMaze = new Maze();
+        String outputFolder = "output/"; //directory to record results
+
+        //testMaze.testValueIteration(outputFolder, 0.99);
+        //testMaze.testPolicyIteration(outputFolder, 0.5);
+        testMaze.testQLearning(outputFolder, 0.99);
+    }
+
+    public void visualizePolicy(QComputablePlanner planner, Policy p){
+        List <State> allStates = StateReachability.getReachableStates(initialState,
+                (SADomain)domain, hashingFactory);
+        LandmarkColorBlendInterpolation rb = new LandmarkColorBlendInterpolation();
+        rb.addNextLandMark(0., Color.RED);
+        rb.addNextLandMark(1., Color.BLUE);
+
+        StateValuePainter2D svp = new StateValuePainter2D(rb);
+        svp.setXYAttByObjectClass(GridWorldDomain.CLASSAGENT, GridWorldDomain.ATTX,
+                GridWorldDomain.CLASSAGENT, GridWorldDomain.ATTY);
+
+        PolicyGlyphPainter2D spp = ArrowActionGlyph.getNSEWPolicyGlyphPainter(GridWorldDomain.CLASSAGENT, GridWorldDomain.ATTX,
+                GridWorldDomain.ATTY, GridWorldDomain.ACTIONNORTH,
+                GridWorldDomain.ACTIONSOUTH, GridWorldDomain.ACTIONEAST, GridWorldDomain.ACTIONWEST);
+
+        spp.setRenderStyle(PolicyGlyphRenderStyle.DISTSCALED);
+
+        ValueFunctionVisualizerGUI gui = new ValueFunctionVisualizerGUI(allStates, svp, planner);
+        gui.setBgColor(Color.WHITE);
+        gui.setSpp(spp);
+        gui.setPolicy(p);
+        gui.initGUI();
+    }
+
+    public void qLearningPlotter(){
+
+        /**
+         * Create factory for Q-learning agent
+         */
+        LearningAgentFactory qLearningFactory = new LearningAgentFactory() {
+
+            @Override
+            public String getAgentName() {
+                return "Q-learning";
+            }
+
+            @Override
+            public LearningAgent generateAgent() {
+                return new QLearning(domain, rf, tf, 0.99, hashingFactory, 0., 0.9);
+            }
+        };
+
+        StateGenerator sg = new ConstantStateGenerator(this.initialState);
+
+        // define experiment
+        LearningAlgorithmExperimenter exp = new LearningAlgorithmExperimenter((SADomain)this.domain, rf, sg,
+                10, 100, qLearningFactory);
+
+        exp.setUpPlottingConfiguration(500, 250, 2, 1000,
+                TrialMode.MOSTRECENTANDAVERAGE,
+                PerformanceMetric.CUMULATIVESTEPSPEREPISODE,
+                PerformanceMetric.AVERAGEEPISODEREWARD);
+
+        // start experiment
+        exp.startExperiment();
+
+        exp.writeStepAndEpisodeDataToCSV("qLearningOutput");
+    }
+
+    public void policyPlotter(){
+
+    }
+
+    public void testValueIteration(String directory, double gamma){
+
+        if(!directory.endsWith("/")){
+            directory = directory + "/";
+        }
+
+        double start = System.nanoTime(), end = 0, runTime = 0;
+
+        OOMDPPlanner planner = new ValueIteration(domain, rf, tf, gamma, hashingFactory, 0.001, 200);
+        planner.planFromState(initialState);
+
+        end = System.nanoTime();
+        runTime = end - start;
+
+        runTime = runTime/10e9;
+        System.out.println("Planner VI time: " + runTime);
+        //create a Q-greedy policy from the planner
+        start = System.nanoTime();
+        Policy p = new GreedyQPolicy((QComputablePlanner)planner);
+
+        //record the plan results to a file
+        EpisodeAnalysis analysis = p.evaluateBehavior(initialState, rf, tf);
+        end = System.nanoTime();
+        runTime = end - start;
+
+        runTime = runTime/10e9;
+        System.out.println("VI greedy evaluation time: " + runTime);
+        LinkedList<State> uniqueStates = new LinkedList<State>();
+        System.out.println("Number of states to termination VI:" + analysis.stateSequence.size());
+        for(State s : analysis.stateSequence)
+        {
+            if(!uniqueStates.contains(s))
+                uniqueStates.add(s);
+        }
+        System.out.println("Unique states in policy VI: "+ uniqueStates.size());
+        System.out.println("Discount reward VI: " + analysis.getDiscountedReturn(0.99));
+        double totalReward = 0;
+
+        for( double r : analysis.rewardSequence)
+        {
+            totalReward += r;
+        }
+        System.out.println("Total reward VI: " + totalReward);
+        analysis.writeToFile(directory + "planResult", sp);
+        if(POLICY_VIS_ON)
+            this.visualizePolicy((QComputablePlanner) planner, p);
+    }
+
+    public void testPolicyIteration(String outputPath, double gamma) {
+
+        if(!outputPath.endsWith("/")){
+            outputPath = outputPath + "/";
+        }
+        double start = System.nanoTime(), end = 0, evalTime = 0;
+        OOMDPPlanner planner = new PolicyIteration(domain, rf, tf, gamma, hashingFactory, 0.001, 100, 1000);
+        planner.planFromState(initialState);
+        end = System.nanoTime();
+        evalTime = end - start;
+
+        evalTime /= Math.pow(10, 9);
+        System.out.println("Planner PI time: " + evalTime);
+        //create a Q-greedy policy from the planner
+        Policy p = new GreedyQPolicy((QComputablePlanner)planner);
+        start = System.nanoTime();
+        //record the plan results to a file
+        EpisodeAnalysis analysis = p.evaluateBehavior(initialState, rf, tf);
+        end = System.nanoTime();
+        evalTime = end - start;
+        evalTime /= Math.pow(10, 9);
+        LinkedList<State> uniqueStates = new LinkedList<State>();
+        System.out.println("Number of states to termination PI:" + analysis.stateSequence.size());
+        for(State s : analysis.stateSequence)
+        {
+            if(!uniqueStates.contains(s))
+                uniqueStates.add(s);
+        }
+        System.out.println("Unique states in policy PI: "+ uniqueStates.size());
+        System.out.println("PI time: " + evalTime);
+        System.out.println("Discount reward PI: " + analysis.getDiscountedReturn(gamma));
+        double totalReward = 0;
+
+        for( double r : analysis.rewardSequence)
+        {
+            totalReward += r;
+        }
+        System.out.println("Total reward PI: " + totalReward);
+        analysis.writeToFile(outputPath + "planResult", sp);
+        if(POLICY_VIS_ON)
+            this.visualizePolicy((QComputablePlanner) planner, p);
+    }
+
+    public void testQLearning(String outputPath, double gamma){
+
+        if(!outputPath.endsWith("/")){
+            outputPath = outputPath + "/";
+        }
+        double start = System.nanoTime(), end = 0, evalTime = 0;
+
+        LearningAgent agent = new QLearning(domain, rf, tf, gamma, hashingFactory, 0., 0.9);
+        //run learning for 100 episodes
+        for(int i = 0; i < 100; i++){
+            agent.runLearningEpisodeFrom(initialState);
+        }
+        end = System.nanoTime();
+        evalTime = end - start;
+        evalTime /= Math.pow(10, 9);
+        System.out.println("Q Learning time: " + evalTime);
+        start = System.nanoTime();
+        Policy p = new GreedyQPolicy((QComputablePlanner)((QLearning)agent));
+
+        EpisodeAnalysis analysis = p.evaluateBehavior(initialState, rf, tf);
+        end = System.nanoTime();
+        evalTime = end - start;
+        evalTime /= Math.pow(10, 9);
+        System.out.println("Q-Learning Eval time: " + evalTime);
+        LinkedList<State> uniqueStates = new LinkedList<State>();
+        System.out.println("Number of states to termination Q-Learning:" + analysis.stateSequence.size());
+        for(State s : analysis.stateSequence)
+        {
+            if(!uniqueStates.contains(s))
+                uniqueStates.add(s);
+        }
+        System.out.println("Unique states in Q-Learning: "+ uniqueStates.size());
+        System.out.println("Q time: " + evalTime);
+        System.out.println("Discount reward Q: " + analysis.getDiscountedReturn(0.99));
+        double totalReward = 0;
+
+        for( double r : analysis.rewardSequence)
+        {
+            totalReward += r;
+        }
+        System.out.println("Total reward Q-Learning: " + totalReward);
+        analysis.writeToFile(outputPath + "planResult", sp);
+        if(POLICY_VIS_ON)
+        {
+            this.visualizePolicy((QComputablePlanner) ((QLearning) agent), p);
+            this.qLearningPlotter();
+        }
+    }
+}
